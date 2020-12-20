858baa4f112e4745ec9c4097d32b9e15bfc101da
Bella Amandine <bellamandy45@gmail.com>
2020-12-20 16:14:34 +0200

    Initialize app

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/amandine/Documents/moringa-school-projects/my-independent-project/core-projects/gallery-app/virtual/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
