62015cf8d9b1202ff6afa3f0b4894a2cf17e1249
Bella Amandine <bellamandy45@gmail.com>
2020-12-20 17:23:18 +0200

    Define category and location models and define the relationships

--- galleryapp/models.py
+++ galleryapp/models.py
@@ -4,4 +4,22 @@ from django.db import models
 class Image(models.Model):
     image = models.ImageField(upload_to = 'images/')
     image_name = models.CharField(max_length = 30)
+    category = models.ForeignKey(Category)
+    location = models.ForeignKey(Location)
 
+    def __str__(self):
+        return self.image_name
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length = 30)
+    category_description = models.TextField()
+
+    def __str__(self):
+        return self.category_name
+
+class Location(models.Model):
+    location_name = models.CharField(max_length = 30)
+
+    def __str__(self):
+        return self.location_name
