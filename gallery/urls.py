d5d0a80f8978a0b2b58ee23b1d5f0ab39ddc05cf
Bella Amandine <bellamandy45@gmail.com>
2020-12-22 18:59:06 +0200

    Add templates, base.html and index.html

--- gallery/urls.py
+++ gallery/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^gallery', include('galleryapp.urls'))
+    url(r'^gallery/', include('galleryapp.urls'))
+    
 ]
--- galleryapp/views.py
+++ galleryapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Image
 
 # Create your views here.
-def welcome(request):
-    return HttpResponse('Welcome')
+def index(request):
+    all_images = Image.get_all_image
+    return render(request, 'index.html', {"images": all_images})
