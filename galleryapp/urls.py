5bad02b798a096d65fb044ae58b86c1042e274d0
Bella Amandine <bellamandy45@gmail.com>
2020-12-23 19:59:30 +0200

    Define search functionnality view route

--- galleryapp/urls.py
+++ galleryapp/urls.py
@@ -4,7 +4,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url('^$', views.index,name='home')
+    url('^$', views.index,name='home'),
+    url(r'^search/category/',views.search_by_category_results, name='search_cat_results'),
+    url(r'^search/location/',views.search_by_location_results, name='search_loc_results'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- galleryapp/views.py
+++ galleryapp/views.py
@@ -6,3 +6,30 @@ from .models import Image
 def index(request):
     all_images = Image.get_all_image
     return render(request, 'index.html', {"images": all_images})
+
+def search_by_category_results(request):
+
+    if 'category' in request.GET and request.GET["category"]:
+        search_term = request.GET.get("category")
+        searched_images = Image.search_image(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all-images/search.html', {"message": message, "images": searched_images})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'all-images/search.html', {"message": message})
+
+def search_by_location_results(request):
+
+    if 'location' in request.GET and request.GET["location"]:
+
+        search_term = request.GET.get("location")
+        searched_images = Image.search_image(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all-images/search.html', {"message": message, "images": searched_images})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'all-images/search.html', {"message": message})
