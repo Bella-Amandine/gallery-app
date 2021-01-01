f75daa64abb1087c9ec444cd0b608be9824700b4
Bella Amandine <bellamandy45@gmail.com>
2021-01-01 17:09:10 +0200

    Add method and page to display images by location

--- galleryapp/urls.py
+++ galleryapp/urls.py
@@ -6,8 +6,10 @@ from . import views
 urlpatterns=[
     url('^$', views.index,name='home'),
     url(r'^all/images/',views.get_all_images, name='all_images'),
+    url(r'^all/images/([a-zA-Z]+)',views.get_all_images_by_location, name='all_images_by_loc'),
     url(r'^search/category/',views.search_by_category_results, name='search_cat_results'),
     url(r'^search/location/',views.search_by_location_results, name='search_loc_results'),
+    url(r'^images/copy/(\d+)',views.copy_image_link, name='copy_image_link'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- galleryapp/views.py
+++ galleryapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Image, Category, Location
+import pyperclip
 
 # Create your views here.
 def index(request):
@@ -10,6 +11,10 @@ def index(request):
 def get_all_images(request):
     all_images = Image.get_all_image()
     return render(request, 'all-images/all-images.html', {"images": all_images})
+
+def get_all_images_by_location(request, location_name):
+    all_images_by_loc = Image.filter_by_location(location_name)
+    return render(request, 'all-images/all-images-by-loc.html', {"images" : all_images_by_loc})
 
 def search_by_category_results(request):
 
@@ -29,7 +34,7 @@ def search_by_location_results(request):
     if 'location' in request.GET and request.GET["location"]:
 
         search_term = request.GET.get("location")
-        searched_images = Image.search_image(search_term)
+        searched_images = Image.filter_by_location(search_term)
         message = f"{search_term}"
 
         return render(request, 'all-images/search.html', {"message": message, "images": searched_images})
@@ -37,3 +42,8 @@ def search_by_location_results(request):
     else:
         message = "You haven't searched for any term"
         return render(request, 'all-images/search.html', {"message": message})
+
+
+def copy_image_link(request, image_id):
+    image_found = Image.get_image_by_id(image_id)
+    pyperclip.copy(image_found.image)
